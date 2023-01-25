30f1c7b6b241cbee1c5317dcbeea61869dfb187f
Riatre Foo <foo@riat.re>
2023-01-25 22:05:05 +0800

    remove obsolote code

--- lyra/sanity_check.py
+++
@@ -1,9 +0,0 @@
-import typer
-
-def main(binary: typer.FileBinaryRead):
-    content = binary.read()
-    assert b"speck" not in content
-    assert b"Speck" not in content
-
-if __name__ == "__main__":
-    typer.run(main)
