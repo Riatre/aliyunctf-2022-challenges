ba7bf01907682a474533f46ab4832119cf270ada
Riatre Foo <foo@riat.re>
2023-01-26 00:21:42 +0800

    lyra: build deploy image & test

---
+++ lyra/test-image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+set -o pipefail
+
+FLAG_PREFIX="aliyunctf{"
+
+zstd -d < image.tar.zst | docker load
+NAME="$(basename $(mktemp /tmp/XXXXXXXX))"
+docker run --rm --name "$NAME" -v "$PWD/flag.txt:/srv/app/flag.txt" -dp 12345:5000 --privileged "$1"
+bye() {
+    docker kill "$NAME"
+}
+trap bye EXIT
+
+echo "cat flag.txt" | python3 solve.py REMOTE HOST=127.0.0.1 PORT=12345 | grep "$FLAG_PREFIX"
