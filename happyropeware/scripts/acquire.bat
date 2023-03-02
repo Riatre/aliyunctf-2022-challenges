cca14e5c63e2d1a23e5921afc678512ea402db1b
Riatre Foo <foo@riat.re>
2023-03-03 04:15:28 +0800

    happyropeware: stop calling CyLR in acquire.bat
    The files it collected are not useful at all. Don't mislead players.

--- happyropeware/scripts/acquire.bat
+++ happyropeware/scripts/acquire.bat
@@ -4,6 +4,4 @@ cd /D "%~dp0"
 
 winpmem_mini_x64_rc2.exe memory.raw
 wmic shadowcopy call create Volume=C:\
-CyLR.exe -of forensic-artifacts.zip -d custom.txt --usnjrnl
 dism /Capture-Image /ImageFile:user-files.wim /CaptureDir:\\?\GLOBALROOT\Device\HarddiskVolumeShadowCopy1\Users /Name:"User Files"
-pause
